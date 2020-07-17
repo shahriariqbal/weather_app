4f2e3002cdcc9c642c74d80bbab8e952c23515a7
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-07-17 09:47:36 +0600

    customized  Title

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,55 @@ import NotFound from './NotFound';
 
 
 
+const AppTitle = styled.h1`
+  display: block;
+  height: 64px;
+  margin: 0;
+  padding: 20px 0;
+  text-align: center;
+  font-size: 40px;
+  font-weight: 300;
+  color: #ffffff;
+  transition: 0.3s 1.4s;
+  opacity: ${({ showLabel }) => (showLabel ? 1 : 0)};
+
+  ${({ secondary }) =>
+    secondary &&
+    `
+    opacity: 1;
+    height: auto;
+    position: relative;
+    padding: 20px 0;
+    font-size: 30px;
+    top: 20%;
+    text-align: center;
+    transition: .5s;
+    @media ${device.tablet} {
+      font-size: 40px;
+    }
+    @media ${device.laptop} {
+      font-size: 50px;
+    }
+    @media ${device.laptopL} {
+      font-size: 60px;
+    }
+    @media ${device.desktop} {
+      font-size: 70px;
+    }
+    
+  `}
+
+  ${({ showResult }) =>
+    showResult &&
+    `
+    opacity: 0;
+    visibility: hidden;
+    top: 10%;
+  `}
+`;
+
+
+
 
 
 
