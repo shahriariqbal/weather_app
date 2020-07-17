1aa99e555b666d77678111d40a69d4082f815a37
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-07-17 20:23:25 +0600

    implement Line chart for wind speed forecast

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -70,7 +70,56 @@ class Chart extends Component{
            
           }
         ]
+      },
+
+
+      chartDataWind:{
+        labels: [ `Date:${props.forecast[0].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}` , 
+                  `Date:${props.forecast[1].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`, 
+                  `Date:${props.forecast[2].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`, 
+                  `Date:${props.forecast[3].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`, 
+                  `Date:${props.forecast[4].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`, 
+                  `Date:${props.forecast[5].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`,
+                  `Date:${props.forecast[6].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`,
+                  `Date:${props.forecast[7].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`,
+                  `Date:${props.forecast[8].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`,
+                  `Date:${props.forecast[9].dt_txt.slice(8, 10)} Time:${ props.forecast[0].dt_txt.slice(11, 13) * 1}`
+                  ],
+        datasets:[
+          {
+            label:'Wind Speed',
+            data:[
+              props.forecast[0].wind.speed,
+              props.forecast[1].wind.speed,
+              props.forecast[2].wind.speed,
+              props.forecast[3].wind.speed,
+              props.forecast[4].wind.speed,
+              props.forecast[5].wind.speed,
+              props.forecast[6].wind.speed,
+              props.forecast[7].wind.speed,
+              props.forecast[8].wind.speed,
+              props.forecast[9].wind.speed,
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(53, 0, 255, 1)',
+              'rgba(255, 0, 86, 1)',
+              'rgba(0, 206, 86, 1)',
+              
+            ],
+            borderColor: '#FFFFFF'
+           
+          }
+        ]
       }
+
+
 
 
     
@@ -109,12 +158,12 @@ class Chart extends Component{
         />
 
         <Line
-          data={this.state.chartData}
+          data={this.state.chartDataWind}
           options={{
             title:{
               color: 'white',
               display:this.props.displayTitle,
-              text:'Forecast Wind in  '+this.props.weather.city,
+              text:'Forecast Wind Speed in  '+this.props.weather.city,
               fontSize:23,
               fontColor: 'white'
              
@@ -131,7 +180,7 @@ class Chart extends Component{
           options={{
             title:{
               display:this.props.displayTitle,
-              text:'Largest Cities In '+this.props.location,
+              text:'Forecast Humidy in '+this.props.weather.city,
               fontSize:25
             },
             legend:{
